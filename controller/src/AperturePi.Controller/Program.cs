8965c97846d8206200b4af2673930697a3a2a1ef
Zachary Yates <zachary.yates@gmail.com>
2018-08-03 16:54:57 -0400

    Added basic implementation of video streaming and ffmpeg example

--- controller/src/AperturePi.Controller/Program.cs
+++ controller/src/AperturePi.Controller/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using OpenCvSharp;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace AperturePi.Controller
 {
@@ -9,7 +12,28 @@ namespace AperturePi.Controller
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // simple test of OpenCV video stream capture
+
+            // this should capture from the default webcam? at least that's what someone said online
+            var capture = new VideoCapture(0);
+
+            using (var output = Console.OpenStandardOutput())
+            using (var window = new Window("capture"))
+            using (var frame = new Mat()) {
+                while (true) {
+                    // gets a frame from the video
+                    capture.Read(frame);
+                    if (frame.Empty())
+                        break;
+
+                    // show in a desktop window for debugging
+                    window.ShowImage(frame);
+                    // pipe the output to vlc for RTSP serving
+                    frame.WriteToStream(output);
+
+                    Cv2.WaitKey(5);
+                }
+            }
         }
     }
 }
