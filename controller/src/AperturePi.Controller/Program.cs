42ce07829c0bcbbbe0cae10ae49c9df15040dd90
Zachary Yates <zachary.yates@gmail.com>
2018-08-03 14:20:34 -0400

    Setup docker and hello world controller project

---
+++ controller/src/AperturePi.Controller/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AperturePi.Controller
+{
+    class Program
+    {
+        // TODO -- setup that handles sigterm
+        // https://dejanstojanovic.net/aspnet/2018/june/clean-service-stop-on-linux-with-net-core-21/
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
