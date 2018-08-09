5517f77bf7d60b93aaa6beecac9683d023bbef84
Zachary Yates <zachary.yates@gmail.com>
2018-08-09 00:03:16 -0400

    Updated opencv code so it plays with the integrated webcam

--- controller/src/app.py
+++ controller/src/app.py
@@ -1,2 +1,16 @@
 import cv2
-print(cv2.__version__)
+import sys
+
+print("Starting stream...")
+# capture the default webcam
+stream = cv2.VideoCapture(0)
+
+while(stream.isOpened()):
+    running, frame = stream.read()
+    if running==True:        
+        sys.stdout.write(frame.tostring())
+    else:
+        break
+
+stream.release()
+print("Stream closed")
